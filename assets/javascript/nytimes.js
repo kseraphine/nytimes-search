b7a2f5737b8ae5c9e1edf5bb601995d4e17b6cf3
Evan Andreolli <eaa864@gmail.com>
2016-08-27 13:07:39 -0500

    initial

---
+++ assets/javascript/nytimes.js
@@ -0,0 +1,20 @@
+var key = "9d4a8986921972b65754ea0809d47c84:12:74623931";
+
+var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + key;
+
+var querylist = "";
+
+
+
+function(){
+	$.ajax({url: queryURL, method: "GET"})
+		
+
+
+
+
+
+
+
+
+}
